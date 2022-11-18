76b180e9e125db6731ff385a1778c447ef0578ad
Juraj Oberta <jurajo80@gmail.com>
2022-11-18 20:46:23 +0100

    Updated routing to welcome and page not found pages.

--- myskin-spa/src/app/app-routing.module.ts
+++ myskin-spa/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './core/component/page-not-found/page-not-found.component';
+import { WelcomeComponent } from './core/component/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: '**', component: PageNotFoundComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
