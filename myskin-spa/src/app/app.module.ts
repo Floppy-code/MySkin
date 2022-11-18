ea22ec11ff0d6ac94093f2d2216b4855ab6c06fc
Juraj Oberta <jurajo80@gmail.com>
2022-11-18 20:35:58 +0100

    Generated inital components

--- myskin-spa/src/app/app.module.ts
+++ myskin-spa/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WelcomeComponent } from './core/component/welcome/welcome.component';
+import { PageNotFoundComponent } from './core/component/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
