6655d013b5536d098328990b1ae4a716763787df
Juraj Oberta <jurajo80@gmail.com>
2023-03-10 21:31:53 +0100

    Implemented better training and K-Fold validation

---
+++ myskin-training/StartKFoldCrossvalidationTraining.ps1
@@ -0,0 +1,9 @@
+#Runs Training on k folds
+$TrainingScriptPath = "C:\Users\juraj\source\repos\MySkin\myskin-training\training\NeuronNetworkTraining.py"
+$PythonEnviromentPath = "C:\Users\juraj\source\enviroments\MySkinTraining\Scripts\python.exe"
+
+Start-Process -Wait -NoNewWindow $PythonEnviromentPath "$TrainingScriptPath 0"
+Start-Process -Wait -NoNewWindow $PythonEnviromentPath "$TrainingScriptPath 1"
+Start-Process -Wait -NoNewWindow $PythonEnviromentPath "$TrainingScriptPath 2"
+Start-Process -Wait -NoNewWindow $PythonEnviromentPath "$TrainingScriptPath 3"
+Start-Process -Wait -NoNewWindow $PythonEnviromentPath "$TrainingScriptPath 4"
