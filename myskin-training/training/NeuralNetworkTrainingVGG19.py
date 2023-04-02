f583ed1bff1042640089ff92078436213cef3802
Juraj Oberta <jurajo80@gmail.com>
2023-04-02 17:45:05 +0200

    Updated training scripts to also calculate Precision and Recall metrics

--- myskin-training/training/NeuralNetworkTrainingVGG19.py
+++ myskin-training/training/NeuralNetworkTrainingVGG19.py
@@ -1,24 +1,30 @@
 import sys
 import tensorflow as tf
 import numpy as np
-from utils.TrainingStatisticsUtils import save_training_statistics
+from utils.TrainingStatisticsUtils import save_training_statistics, precision_m, recall_m
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.applications import VGG19
 from tensorflow.keras.layers import Input, Dense, Flatten, AveragePooling2D
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.losses import CategoricalCrossentropy
 from sklearn.model_selection import StratifiedKFold
 
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
+fold = sys.argv[1]
+lr_str = '1e-' + sys.argv[2]
+lr_float = float(lr_str)
+
 # ===== CONSTANS =====
 # Model
-MODEL_NAME = 'resnet50_lr1e-7_not_augumented'
+MODEL_NAME = 'vgg19_lr' + lr_str + '_not_augumented_transfer'
 MODEL_ARCHITECTURE_NAME = 'VGG19_no_aug'
 TRAINING_STATISTICS_ACCURACY_FILE = './training/stats/network_training_acc.csv'
 TRAINING_STATISTICS_VAL_ACCURACY_FILE = './training/stats/network_training_val_acc.csv'
+TRAINING_STATISTICS_VAL_PRECISION_FILE = './training/stats/network_training_val_prec.csv'
+TRAINING_STATISTICS_VAL_RECALL_FILE = './training/stats/network_training_val_rec.csv'
 TRAINING_STATISTICS_LOSS_FILE = './training/stats/network_training_loss.csv'
 TRAINING_STATISTICS_VAL_LOSS_FILE = './training/stats/network_training_val_loss.csv'
 
@@ -28,17 +34,16 @@ LABEL_FILE = './resources/labels.npy'
 PREPROCESSED_FEATURE_FILE = f'./resources/{MODEL_ARCHITECTURE_NAME}_features.npy'
 PREPROCESSED_LABEL_FILE = f'./resources/{MODEL_ARCHITECTURE_NAME}_labels.npy'
 
-fold = sys.argv[1]
-
 X = np.load(open(PREPROCESSED_FEATURE_FILE, 'rb'))
 Y = np.load(open(PREPROCESSED_LABEL_FILE, 'rb'))
 
 # Create class_weights for unbalanced dataset classes
 from sklearn.utils import class_weight
 
+y_integers = np.argmax(Y, axis=1)
 class_weights = class_weight.compute_class_weight(class_weight="balanced",
-                                                  classes=np.unique(np.squeeze(Y)),
-                                                  y=np.squeeze(Y))
+                                                  classes=np.unique(y_integers),
+                                                  y=y_integers)
 class_weights = dict(enumerate(class_weights))
 
 # Split into 5 folds evenly
@@ -46,7 +51,7 @@ skf = StratifiedKFold(n_splits=5, random_state=None, shuffle=False)
 
 train_indexes = []
 test_indexes = []
-for i, (train_index, test_index) in enumerate(skf.split(X, Y)):
+for i, (train_index, test_index) in enumerate(skf.split(X, y_integers)):
     train_indexes.append(train_index)
     test_indexes.append(test_index)
 
@@ -62,9 +67,9 @@ earlyStopping = EarlyStopping(monitor='val_loss',
 model = Sequential()
 vgg19 = VGG19(include_top=False,
              input_shape=(128, 128, 3),
-             weights=None,
+             weights='imagenet',
              pooling='max')
-vgg19.trainable = True
+vgg19.trainable = False
 
 model.add(vgg19)
 
@@ -74,16 +79,16 @@ model.add(Dense(4096, activation='relu'))
 model.add(Dense(4096, activation='relu'))
 model.add(Dense(7, activation='softmax'))
 
-model.compile(optimizer=Adam(learning_rate=1e-7),
-              loss=SparseCategoricalCrossentropy(),
-              metrics=['accuracy'])
+model.compile(optimizer=Adam(learning_rate=lr_float),
+              loss=CategoricalCrossentropy(),
+              metrics=['accuracy', recall_m, precision_m])
 
 model.summary(expand_nested=True)
 
 history = model.fit(X[train_index], Y[train_index],
                     class_weight=class_weights,
                     epochs=200,
-                    batch_size=96,
+                    batch_size=128,
                     validation_data=(X[test_index], Y[test_index]),
                     callbacks=[earlyStopping])
 
@@ -91,4 +96,6 @@ save_training_statistics(TRAINING_STATISTICS_ACCURACY_FILE, history.history['acc
 save_training_statistics(TRAINING_STATISTICS_LOSS_FILE, history.history['loss'], MODEL_NAME, fold)
 save_training_statistics(TRAINING_STATISTICS_VAL_ACCURACY_FILE, history.history['val_accuracy'], MODEL_NAME, fold)
 save_training_statistics(TRAINING_STATISTICS_VAL_LOSS_FILE, history.history['val_loss'], MODEL_NAME, fold)
+save_training_statistics(TRAINING_STATISTICS_VAL_PRECISION_FILE, history.history['val_precision_m'], MODEL_NAME, fold)
+save_training_statistics(TRAINING_STATISTICS_VAL_RECALL_FILE, history.history['val_recall_m'], MODEL_NAME, fold)
 #===== MODEL SPACE =====
--- myskin-training/training/utils/TrainingStatisticsUtils.py
+++ myskin-training/training/utils/TrainingStatisticsUtils.py
@@ -1,4 +1,21 @@
 import os.path
+import keras.backend as K
+
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)), axis=0)
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)), axis=0)
+    recall = true_positives / (possible_positives + K.epsilon())
+    recall = K.mean(recall)
+    return recall
+
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)), axis=0)
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)), axis=0)
+    precision = true_positives / (predicted_positives + K.epsilon())
+    precision = K.mean(precision)
+    return precision
 
 
 def save_training_statistics(file_path, data_to_save, model_name, model_fold):
